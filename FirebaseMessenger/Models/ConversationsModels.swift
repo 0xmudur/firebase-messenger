ab33b138e05ae65278494abfe98a1774327afe6d
mudur <muhamm3dea@gmail.com>
2021-02-01 23:46:37 +0300

    GIF Added

---
+++ FirebaseMessenger/Models/ConversationsModels.swift
@@ -0,0 +1,21 @@
+//
+//  ConversationModel.swift
+//  FirebaseMessenger
+//
+//  Created by Muhammed Emin Aydın on 24.01.2021.
+//
+
+import Foundation
+
+struct Conversation {
+    let id: String
+    let name: String
+    let otherUserEmail: String
+    let latestMessage: LatestMessage
+}
+
+struct LatestMessage {
+    let date: String
+    let text: String
+    let isRead: Bool
+}
